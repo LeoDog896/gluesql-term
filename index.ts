b4705436f907bf2fdb3a8dea1f5af6b9756ac2f1
Tristan F <LeoDog896@hotmail.com>
2023-03-12 14:39:38 -0400

    feat: sql commands

--- index.ts
+++ index.ts
@@ -1,12 +1,50 @@
 import "xterm/css/xterm.css";
 import { Terminal } from "xterm";
 import { FitAddon } from 'xterm-addon-fit';
+import { gluesql } from 'gluesql/gluesql.rollup';
 
+const db = await gluesql();
 const terminal = document.querySelector<HTMLDivElement>("#terminal")!;
-
 const term = new Terminal();
+
 const fitAddon = new FitAddon();
 term.loadAddon(fitAddon);
 term.open(terminal);
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
 fitAddon.fit();
+
+let command = '';
+
+function prompt() {
+    term.write('\n\r');
+    term.write('gluesql> ');
+}
+
+async function runCommand() {
+    let result: any
+    try {
+        result = await db.query(command);
+        term.write(JSON.stringify(result));
+    } catch (e) {
+        result = e;
+        console.error(e);
+        term.write(result.toString().replaceAll('\n', '\n\r'));
+    }
+    command = '';
+}
+
+term.onKey(async e => {
+    term.write(e.key);
+    if (e.key == '\r') {
+        term.write('\n');
+        await runCommand();
+        prompt();
+    } else if (e.key == '\u0003') {
+        prompt();
+    } else if (e.key == '\u007F') {
+        prompt();
+    } else {
+        command += e.key;
+    }
+})
+
+prompt();
